076e91de4e291c86c87ca418bbee99c4b0f919bf
Mathieu Lagacé <mathieu.lagace@shopify.com>
2024-06-19 13:25:02 -0400

    Query the metafield value

--- extensions/customer-preference/src/ProfilePreferenceExtension.tsx
+++ extensions/customer-preference/src/ProfilePreferenceExtension.tsx
@@ -6,20 +6,66 @@ import {
   Text,
 } from "@shopify/ui-extensions-react/customer-account";
 
-export default reactExtension("customer-account.profile.block.render", () => (
-  <ProfilePreferenceExtension />
-));
+export default reactExtension(
+  "customer-account.profile.block.render",
+  async () => {
+    const customerPreferences = await getCustomerPreferences();
 
-function ProfilePreferenceExtension() {
+    console.log(customerPreferences);
+
+    return (
+      <ProfilePreferenceExtension
+        clothingCategory={customerPreferences.clothingCategory}
+      />
+    );
+  },
+);
+
+interface Props {
+  clothingCategory?: string;
+}
+
+function ProfilePreferenceExtension(props: Props) {
   return (
     <Card padding>
       <BlockStack spacing="loose">
         <Heading level={3}>Preferences</Heading>
         <BlockStack spacing="none">
-          <Text appearance="subdued">Clothing Category</Text>
-          <Text>TODO</Text>
+          <Text appearance="subdued">Clothing category</Text>
+          <Text>{props.clothingCategory}</Text>
         </BlockStack>
       </BlockStack>
     </Card>
   );
+}
+
+async function getCustomerPreferences() {
+  const response = await fetch(
+    "shopify:customer-account/api/2024-07/graphql.json",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `query preferences($namespace: String!, $key: String!) {
+          customer {
+            metafield(namespace: $namespace, key: $key) {
+              value
+            }
+          }
+        }`,
+        variables: {
+          key: "clothing-category",
+          namespace: "$app:preferences",
+        },
+      }),
+    },
+  );
+
+  const { data } = await response.json();
+
+  return {
+    clothingCategory: data.customer.metafield?.value,
+  };
 }
