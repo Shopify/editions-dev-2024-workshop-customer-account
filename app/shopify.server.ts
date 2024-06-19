035f1251d71ae7a4917f2b764d666be632723c92
Mathieu Lagacé <mathieu.lagace@shopify.com>
2024-06-19 13:22:41 -0400

    Create the metafield definition

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -8,11 +8,13 @@ import {
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import { restResources } from "@shopify/shopify-api/rest/admin/2024-04";
 import prisma from "./db.server";
+import type { AdminApiContext } from "node_modules/@shopify/shopify-app-remix/dist/ts/server/clients";
+import type { ShopifyRestResources } from "@shopify/shopify-api";
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  apiVersion: ApiVersion.April24,
+  apiVersion: ApiVersion.Unstable,
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
@@ -26,8 +28,24 @@ const shopify = shopifyApp({
     },
   },
   hooks: {
-    afterAuth: async ({ session }) => {
-      shopify.registerWebhooks({ session });
+    afterAuth: async ({ admin, session }) => {
+      await shopify.registerWebhooks({ session });
+
+      try {
+        const metafield = await getMetafieldDefinition(admin);
+
+        if (metafield == null) {
+          await createMetafieldDefinition(admin);
+        }
+      } catch (error: any) {
+        if ("graphQLErrors" in error) {
+          console.error(error.graphQLErrors);
+        } else {
+          console.error(error);
+        }
+
+        throw error;
+      }
     },
   },
   future: {
@@ -46,3 +64,66 @@ export const unauthenticated = shopify.unauthenticated;
 export const login = shopify.login;
 export const registerWebhooks = shopify.registerWebhooks;
 export const sessionStorage = shopify.sessionStorage;
+
+async function getMetafieldDefinition(
+  admin: AdminApiContext<ShopifyRestResources>,
+) {
+  const response = await admin.graphql(getMetafieldDefinitionQuery, {
+    variables: {
+      key: "clothing-category",
+      namespace: "$app:preferences",
+      ownerType: "CUSTOMER",
+    },
+  });
+
+  const json = await response.json();
+  return json.data?.metafieldDefinitions.nodes[0];
+}
+
+const getMetafieldDefinitionQuery = `#graphql
+query getMetafieldDefinition($key: String!, $namespace: String!, $ownerType: MetafieldOwnerType!) {
+  metafieldDefinitions(first: 1, key: $key, namespace: $namespace, ownerType: $ownerType) {
+    nodes {
+      id
+    }
+  }
+}
+`;
+
+async function createMetafieldDefinition(
+  admin: AdminApiContext<ShopifyRestResources>,
+) {
+  const response = await admin.graphql(createMetafieldDefinitionMutation, {
+    variables: {
+      definition: {
+        access: {
+          customerAccount: "READ_WRITE",
+          admin: "PRIVATE",
+        },
+        key: "clothing-category",
+        name: "The customer's preferred clothing category",
+        namespace: "$app:preferences",
+        ownerType: "CUSTOMER",
+        type: "single_line_text_field",
+      },
+    },
+  });
+
+  const json = await response.json();
+  console.log(JSON.stringify(json, null, 2));
+}
+
+const createMetafieldDefinitionMutation = `#graphql
+mutation metafieldDefinitionCreate($definition: MetafieldDefinitionInput!) {
+  metafieldDefinitionCreate(definition: $definition) {
+    createdDefinition {
+      key
+      namespace
+    }
+    userErrors {
+      field
+      message
+    }
+  }
+}
+`;
